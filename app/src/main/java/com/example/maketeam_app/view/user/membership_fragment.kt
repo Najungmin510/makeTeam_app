87aaf4b3cbe5a48687b2f7cb5f2a7a01ebd3e189
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-06 16:14:09 +0900

    add : 회원가입 화면 추가

---
+++ app/src/main/java/com/example/maketeam_app/view/user/membership_fragment.kt
@@ -0,0 +1,21 @@
+package com.example.maketeam_app.view.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.maketeam_app.R
+
+class membership_fragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_membership)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

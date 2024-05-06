3545d568953e7e2b7192fac2ce593e71487defb8
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-06 16:13:44 +0900

    add : baseFragment추가

---
+++ app/src/main/java/com/example/maketeam_app/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.example.maketeam_app.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+
+/**fragment()대신 이걸 상속받으시면 됩니다.*/
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes : Int) : Fragment() {
+    lateinit var binding : T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.lifecycleOwner = viewLifecycleOwner
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    abstract fun initView()
+
+    abstract fun initClick()
+
+}

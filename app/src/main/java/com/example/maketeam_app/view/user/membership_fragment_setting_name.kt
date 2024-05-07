e9617702bc78a72535e6e94a4c2d0630cc70de41
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-07 23:30:50 +0900

    fix : 사용자 정보 세팅 화면 수정

--- app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_setting_name.kt
+++ app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_setting_name.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.maketeam_app.R
 
-class membership_fragment : AppCompatActivity() {
+class membership_fragment_setting_name : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.fragment_membership)
+        setContentView(R.layout.fragment_member_setting_name)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_setting_school.kt
@@ -0,0 +1,16 @@
+package com.example.maketeam_app.view.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.maketeam_app.R
+
+class membership_fragment_setting_school : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_membership_setting_school)
+
+    }
+}

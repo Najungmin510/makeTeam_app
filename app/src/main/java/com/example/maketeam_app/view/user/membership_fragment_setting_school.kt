443d003e02ffbb18ff9eb7331f4f53a3c0000ea0
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-08 22:21:43 +0900

    add : 일부 텍스트에 색깔 추가

--- app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_setting_school.kt
+++ app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_setting_school.kt
@@ -1,16 +1,32 @@
 package com.example.maketeam_app.view.user
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.maketeam_app.R
+import com.example.maketeam_app.databinding.FragmentMembershipSettingSchoolBinding
 
 class membership_fragment_setting_school : AppCompatActivity() {
+
+    private lateinit var binding : FragmentMembershipSettingSchoolBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_membership_setting_school)
+        binding = FragmentMembershipSettingSchoolBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val school = SpannableStringBuilder(binding.titleSettingSchool.text.toString())
+        school.apply {
+            setSpan(ForegroundColorSpan(resources.getColor(R.color.main_color)), 10, 12, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        binding.titleSettingSchool.text = school
 
     }
 }

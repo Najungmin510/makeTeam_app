36621979982e2e558ad879bdf7c970a244392029
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-08 21:40:27 +0900

    add : 카카오 로그인 구현 완료

---
+++ app/src/main/java/com/example/maketeam_app/base/Application.kt
@@ -0,0 +1,11 @@
+package com.example.maketeam_app.base
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+class Application : Application() {
+    override fun onCreate(){
+        super.onCreate()
+
+        KakaoSdk.init(this, "87a433ac0f324bdc908ff8636bce1a72")
+    }
+}
--- app/src/main/java/com/example/maketeam_app/view/login_fragment.kt
+++ app/src/main/java/com/example/maketeam_app/view/login_fragment.kt
@@ -1,21 +1,60 @@
 package com.example.maketeam_app.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.maketeam_app.R
+import com.example.maketeam_app.databinding.FragmentLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 class login_fragment : AppCompatActivity() {
+
+    private lateinit var binding : FragmentLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.fragment_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnLogin.setOnClickListener {
+
+            Log.d("login","로그인 버튼 클릭")
+            
+            val callback : (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if(error != null){
+                    Log.d("login","카카오 계정으로 로그인 실패")
+                } else {
+                    Log.d("login", "카카오 계정으로 로그인 성공")
+                }
+            }
+            
+            /**카카오톡으로 로그인*/
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if(error != null){
+                        Log.d("login", "카카오톡으로 로그인 실패")
+                        if(error is ClientError && error.reason == ClientErrorCause.Cancelled){
+                            return@loginWithKakaoTalk
+                        }
+
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.d("login","카카오톡으로 로그인 성공")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+
+        } //로그인 버튼
+
+
     }
 }

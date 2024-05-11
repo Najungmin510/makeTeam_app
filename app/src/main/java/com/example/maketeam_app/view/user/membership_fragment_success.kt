4ccb58c36c8831393e1bb4ebe195cb9671cd9870
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-11 16:50:42 +0900

    add : 회원정보 기입, 완료 창 배치

---
+++ app/src/main/java/com/example/maketeam_app/view/user/membership_fragment_success.kt
@@ -0,0 +1,16 @@
+package com.example.maketeam_app.view.user
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.maketeam_app.R
+
+class membership_fragment_success : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_membership_success)
+
+    }
+}

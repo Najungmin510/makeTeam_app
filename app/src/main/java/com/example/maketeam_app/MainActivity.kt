552e3fc31c78620be6966056766eceb35e3ef9a3
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-23 19:51:06 +0900

    add : 바텀 네비게이션 아이콘 추가

--- app/src/main/java/com/example/maketeam_app/MainActivity.kt
+++ app/src/main/java/com/example/maketeam_app/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.maketeam_app.databinding.ActivityMainBinding
 import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val hash = Utility.getKeyHash(this)
-        Log.d("Hash", hash)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val hash = Utility.getKeyHash(this)
+        //Log.d("Hash", hash)
+
+        binding.bottomNav.itemIconTintList = null
+
+
+
 
     }
 }

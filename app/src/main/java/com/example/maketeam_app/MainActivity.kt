ad4d2dbc5971d9c24c60a73bf8c1d5c8b509bfb2
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-06-01 23:15:14 +0900

    add: navigtaion 추가

--- app/src/main/java/com/example/maketeam_app/MainActivity.kt
+++ app/src/main/java/com/example/maketeam_app/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.maketeam_app
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.maketeam_app.databinding.ActivityMainBinding
 import com.kakao.sdk.common.util.Utility
 
@@ -18,11 +21,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNav.itemIconTintList = null
+        navControllerSetting()
+
+
         //val hash = Utility.getKeyHash(this)
         //Log.d("Hash", hash)
+    }
 
-        binding.bottomNav.itemIconTintList = null
+    private fun navControllerSetting(){
+        val navHostFragment = supportFragmentManager.
+        findFragmentById(R.id.container_fragment) as NavHostFragment
 
+        val navController = navHostFragment.navController
 
+        val bottomNavigationView = binding.bottomNav
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun noShowNavigation(){
+        binding.bottomNav.visibility = View.GONE
+    }
+
+    fun noShowTabLayout(){
+        binding.tabLayoutMain.visibility = View.GONE
     }
 }

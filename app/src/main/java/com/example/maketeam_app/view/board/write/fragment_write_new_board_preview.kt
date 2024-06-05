7d98fcae7fe0fad46c914e7f7e85b6687b6ba441
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-06-05 22:14:19 +0900

    add : 게시물 작성 미리보기 추가

---
+++ app/src/main/java/com/example/maketeam_app/view/board/write/fragment_write_new_board_preview.kt
@@ -0,0 +1,25 @@
+package com.example.maketeam_app.view.board.write
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.maketeam_app.R
+import com.example.maketeam_app.base.BaseFragment
+import com.example.maketeam_app.databinding.FragmentWriteNewBoardPreviewBinding
+
+
+/**
+ * 업로드 하기 전 미리보기
+ * */
+class fragment_write_new_board_preview : BaseFragment<FragmentWriteNewBoardPreviewBinding>(R.layout.fragment_write_new_board_preview) {
+    override fun initView() {
+
+    }
+
+    override fun initClick() {
+
+    }
+
+}

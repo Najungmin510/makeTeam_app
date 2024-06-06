29351472af6290eff35cd3926ce9f6e516951de4
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-06-06 23:14:00 +0900

    add : viewmodel, repository 추가

---
+++ app/src/main/java/com/example/maketeam_app/access/Repository.kt
@@ -0,0 +1,14 @@
+package com.example.maketeam_app.access
+
+import com.example.maketeam_app.db.DBDao
+import com.example.maketeam_app.model.BoardContent
+import javax.inject.Inject
+
+class Repository @Inject constructor(private val dbDao: DBDao) {
+
+    private val LOG = "repository"
+
+    fun boardInsert(boardContent: BoardContent) {
+        dbDao.insertBoard(boardContent)
+    }
+}
---
+++ app/src/main/java/com/example/maketeam_app/access/ViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.maketeam_app.access
+
+import androidx.lifecycle.AndroidViewModel
+import com.example.maketeam_app.base.Application
+import com.example.maketeam_app.model.BoardContent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModel @Inject constructor(private val repository: Repository, application: Application) : AndroidViewModel(application) {
+    private val LOG = "viewModel"
+
+    fun boardInsert(boardContent: BoardContent){
+        repository.boardInsert(boardContent)
+    }
+
+}

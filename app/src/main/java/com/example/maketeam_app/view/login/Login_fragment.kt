7825605fd1d3ccf2c5b60335bf7820420f801b13
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-06-13 17:53:22 +0900

    fix : 로그인 부분 주석 해제

--- app/src/main/java/com/example/maketeam_app/view/login/Login_fragment.kt
+++ app/src/main/java/com/example/maketeam_app/view/login/Login_fragment.kt
@@ -28,38 +28,37 @@ class login_fragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_logi
 
     override fun initClick() {
         binding.btnLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_login_fragment_to_fragment_membership_setting_name)
-//            Log.d("login","로그인 버튼 클릭")
-//
-//            val callback : (OAuthToken?, Throwable?) -> Unit = { token, error ->
-//                if(error != null){
-//                    Log.d("login","카카오 계정으로 로그인 실패")
-//                    Log.d("로그인 에러", error.toString())
-//                } else {
-//                    Log.d("login", "카카오 계정으로 로그인 성공")
-//                }
-//            }
-//
-//            /**카카오톡으로 로그인*/
-//            if(UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
-//
-//                UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
-//                    if(error != null){
-//                        Log.d("login", "카카오톡으로 로그인 실패")
-//                        if(error is ClientError && error.reason == ClientErrorCause.Cancelled){
-//                            return@loginWithKakaoTalk
-//                        }
-//
-//                        UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
-//                    } else if (token != null) {
-//                        Toast.makeText(requireContext(), "카카오로 로그인 성공", Toast.LENGTH_SHORT).show()
-//                        findNavController().navigate(R.id.action_login_fragment_to_fragment_membership_setting_name)
-//                        Log.d("login","카카오톡으로 로그인 성공")
-//                    }
-//                }
-//            } else {
-//                UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
-//            }
+            Log.d("login","로그인 버튼 클릭")
+
+            val callback : (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if(error != null){
+                    Log.d("login","카카오 계정으로 로그인 실패")
+                    Log.d("로그인 에러", error.toString())
+                } else {
+                    Log.d("login", "카카오 계정으로 로그인 성공")
+                }
+            }
+
+            /**카카오톡으로 로그인*/
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+
+                UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                    if(error != null){
+                        Log.d("login", "카카오톡으로 로그인 실패")
+                        if(error is ClientError && error.reason == ClientErrorCause.Cancelled){
+                            return@loginWithKakaoTalk
+                        }
+
+                        UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+                    } else if (token != null) {
+                        Toast.makeText(requireContext(), "카카오로 로그인 성공", Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.action_login_fragment_to_fragment_membership_setting_name)
+                        Log.d("login","카카오톡으로 로그인 성공")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+            }
 
         } //로그인 버튼
     }

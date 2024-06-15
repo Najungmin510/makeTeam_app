d49bf33c2cb77fd444080cfaff6cfb718e7593ab
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-06-15 22:37:19 +0900

    fix : xml 어색한 부분 수정

--- app/src/main/java/com/example/maketeam_app/view/mypage/fragment_apply_user_document.kt
+++ app/src/main/java/com/example/maketeam_app/view/mypage/fragment_apply_user_document.kt
@@ -16,6 +16,8 @@ class fragment_apply_user_document : BaseFragment<FragmentApplyUserDocumentBindi
         binding.includeMystackDocument.stackTitle.text = "지원자 스택"
         binding.includeMystackDocument.teamSituationDetailBack.visibility = View.INVISIBLE
 
+        binding.includeUserInfoDocument.textUserNameMyinfo.text = "박수호(3.8점)"
+
         binding.includeUserInfoDocument.btnMyinfoEdit.visibility = View.INVISIBLE
         binding.includeUserInfoDocument.applyDetailTitle.text = "김성우 교수님의 HCI 팀원 찾습니다."
         binding.includeMessageDocument.etMyMessage.setText("안녕하세요. 해당 공고에 지원하고 싶습니다. 지원 포지션은 아무거나 상관없어 표시하지 않았습니다.")
@@ -29,7 +31,7 @@ class fragment_apply_user_document : BaseFragment<FragmentApplyUserDocumentBindi
 
     override fun initClick() {
         binding.btnGetUserPhonenum.setOnClickListener { //전화번호 복사하기 버튼 누르면 전화화면으로 이동하게 하기
-            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:01012345678"))
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:01011112222"))
             startActivity(intent)
         }
     }
